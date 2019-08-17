a366f65b4e722192197dde671c1ea3bc4d82170c
CorinnaGeiselhart <Corinna@bayern-mail.de>
2019-08-17 16:55:42 +0200

    Jetzt: Filter für die Suche von neuen Study Groups (Activity und Layout)

---
+++ app/src/main/java/com/example/studygroups/FilterFindGroups.java
@@ -0,0 +1,53 @@
+package com.example.studygroups;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+public class FilterFindGroups extends AppCompatActivity {
+
+    CheckBox monday, tuesday,wednesday,thursday, friday,saturday, sunday;
+    Spinner modulePicker;
+    Button searchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter_find_group);
+
+        findViews();
+        initButton();
+    }
+
+    private void initButton() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get, which checkbox is selected
+                //Intent Daten mitgeben
+
+                Intent intent = new Intent();
+                finish();
+            }
+        });
+    }
+
+    private void findViews() {
+        monday = findViewById(R.id.checkBox_Mo);
+        tuesday = findViewById(R.id.checkBox_Tue);
+        wednesday = findViewById(R.id.checkBox_Wed);
+        thursday = findViewById(R.id.checkBox_Thur);
+        friday = findViewById(R.id.checkBox_Fr);
+        saturday = findViewById(R.id.checkBox_Sat);
+        sunday = findViewById(R.id.checkBox_Sun);
+
+        modulePicker = findViewById(R.id.spinner_Module);
+        searchButton = findViewById(R.id.button_Search);
+    }
+
+
+}
