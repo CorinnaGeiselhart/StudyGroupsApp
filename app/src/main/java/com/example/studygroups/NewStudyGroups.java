481029f7db585e2fd277b023ec9e3ed81f46ad29
Juliana Zgryzek <z.juliana@gmx.de>
2019-09-13 14:48:21 +0200

    Datenstruktur holen-funktionsfähig!!!!

--- app/src/main/java/com/example/studygroups/NewStudyGroups.java
+++ app/src/main/java/com/example/studygroups/NewStudyGroups.java
@@ -1,22 +1,15 @@
 package com.example.studygroups;
 
-import android.util.Log;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class NewStudyGroups extends MainActivityFragment {
 
