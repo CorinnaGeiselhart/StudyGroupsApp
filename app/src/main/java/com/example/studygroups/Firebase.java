c487be205ba7f1862f8864e02733bd2c87c6021c
Juliana Zgryzek <z.juliana@gmx.de>
2019-09-15 14:12:43 +0200

    Picture frei wählbar

--- app/src/main/java/com/example/studygroups/Firebase.java
+++ app/src/main/java/com/example/studygroups/Firebase.java
@@ -1,7 +1,25 @@
 package com.example.studygroups;
 
-public class Firebase {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public abstract class Firebase {
 
+    private static FirebaseAuth mAuth;
+    private static FirebaseUser currentUser;
+
+    public Firebase(){
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+    }
+
+    public static FirebaseUser getCurrentUser (){
+        return currentUser;
+    }
+
+    public static FirebaseAuth getFirebaseAuth(){
+        return mAuth;
+    }
 
 }
